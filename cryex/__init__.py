45f3821ff2c85930f7166cc8cfbaa382c815a7db
walkr <walkr@local>
2016-06-29 15:29:25 -0700

    Bump version

--- cryex/__init__.py
+++ cryex/__init__.py
@@ -6,6 +6,6 @@ from .core import ClientError
 
 __author__ = 'Tony Walker'
 __email__ = 'walkr.walkr@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.20'
 
 __all__ = ['Poloniex', 'Kraken', 'ClientError']
